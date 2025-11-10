ec4deec0d9bfb653e29b47b664da1165a378d20a
Chaoyang Fang <fchy@outlook.com>
2025-11-09 22:13:29 -0500

    remove unused reference for the time being

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hmm_console/core/navigation/router.dart';
 import 'package:hmm_console/core/theme/theme.dart';
